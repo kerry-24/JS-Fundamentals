67f3e6b37e66e8f3321dce474e7e4e96216cc7c5
kerry <Careyk896>
2025-05-22 11:23:15 +0100

    Checking value of argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,9 @@
+//get the command line arguments
 const args = process.argv.slice(2);
+//check if the first argument exist
 if (args[0] === undefined) {
     console.log("No argument");
 }
 else {
-    console.log([0]);
+    console.log(args[0]);
 }
