810334ea207e871d7a659bd9bd054f157e06ef2f
kerry <Careyk896>
2025-05-22 11:04:41 +0100

    Added arguments file

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,9 @@
-const args = process.argv.slice(2);
+const args = process.argv[0];
 if (args.length === 0) {
     console.log("No argument");
 }
 else if (args.length === 1) {
-    console.log("Arguments found");
+    console.log("Argument found");
 }
 else {
     console.log("Arguments found");
